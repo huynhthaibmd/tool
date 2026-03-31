fb9cc51b09a8320dfdb3eb67428543e863f0dfe4
huynhthaibmd <huynhngoanhthai.bmd@gmail.com>
2026-03-31 16:49:09 +0700

    add code tool hojc tap

--- c++/code.cpp
+++ c++/code.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 int main() {
+    int n, W;
+    cin >> n >> W;
 
-    int n;
-    cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    int max = a[0];
-    for (int i = 0; i < n; i++) {
-        if (a[i] > max) {
-            max = a[i];
-        }
-    }
-    cout << "Max: " << max << "\n";
+    vector<int> w(n+1), v(n+1);
+    
+    for (int i = 1; i <= n; i++) cin >> w[i] >> v[i];
 
+
+    vector<int> dp(W+1, 0);
+    for (int i = 1; i <= n; i++)
+        for (int j = W; j >= w[i]; j--)  // duyệt NGƯỢC
+            dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+
+
+    cout << "dp[W]: " << dp[W] << endl;
     return 0;
 }
---
+++ htmlToPDF/run.sh
@@ -0,0 +1,2 @@
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://iotmakervn.github.io/iot-starter-book/
+wkhtmltopdf index.html book.pdf
